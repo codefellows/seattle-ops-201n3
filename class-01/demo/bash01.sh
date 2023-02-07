e292230bb4064eeda1163a4206de36ff74059bbe
CourtHans <cornta@yahoo.com>
2023-02-06 20:26:12 -0800

    Test commit

---
+++ class-01/demo/bash01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Courtney Hans
+# Date of latest revision: 2/14/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "This is my first bash script."
+
+# End
+# Resources: https://codefellows.github.io/ops-201-guide/curriculum/class-02/challenges/demo/bash.html

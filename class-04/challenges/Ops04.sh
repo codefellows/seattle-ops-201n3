46084a93344bb89a49116787992736fc5d2ea34f
CourtHans <cornta@yahoo.com>
2023-02-21 20:54:26 -0800

    class demo script

---
+++ class-04/challenges/Ops04.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script:                       
+# Author:                       
+# Date of latest revision:      
+# Purpose:                  Creates four directories: dir1, dir2, dir3, dir4
+#                            Put the names of the four directories in an array
+#                            References the array variable to create a new .txt file in each directory       
+
+
+# Create four directories (hint us mkdir)
+
+
+
+# put the names of four directories in an array
+
+
+
+# # Declare global variables
+files_array=("f1.txt" "f2.txt" "f3.txt" "f4.txt" "f5.txt")
+
+# # Declare functions
+
+# # Main
+touch challenges/${files_array[4]}
+echo ${files_array[3]}
+echo ${files_array[2]}
+echo ${files_array[1]}
+echo ${files_array[0]}
+
